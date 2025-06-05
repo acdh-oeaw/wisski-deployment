9fde2c503d055c60d0823ddecf176c23374d0b7a
Kevin Stadler <kevinstadler@users.noreply.github.com>
2025-06-05 13:13:51 +0200

    fix: add r11.eu to Drupal settings trusted host patterns

--- config/drupal-context/settings.php
+++ config/drupal-context/settings.php
@@ -730,6 +730,7 @@ $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
  */
  $settings['trusted_host_patterns'] = array(
    '^.+\.acdh-dev\.oeaw\.ac\.at$',
+   '^.+\.r11\.eu$',
  );
 /**
  * The default list of directories that will be ignored by Drupal's file API.
